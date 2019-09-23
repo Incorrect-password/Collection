3088f67c6ff39e738a4c83301c0ab0fccc9d1422
James Taylor <taylor-j12@hotmail.com>
2019-09-23 14:32:17 +0100

    added empty test folders, boilerplate for index page

---
+++ test/index.php
@@ -0,0 +1,17 @@
+<?php
+$db = new PDO('mysql:host:dbname=Collection','root','password');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+         <link href="normalize.css" rel="stylesheet" type="text/css">
+     <title>My Mushroom Collection</title>
+    </head>
+    <body>
+
+    </body>
+</html>
