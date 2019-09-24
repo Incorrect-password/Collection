c9f82e62072d907dd84b1e9f84384d7be9d6dec5
James Taylor <taylor-j12@hotmail.com>
2019-09-24 15:53:33 +0100

    missing file from previous commit

--- index.php
+++ index.php
@@ -9,15 +9,4 @@ echo $output;
 
 ?>
 
-<!--<!DOCTYPE html>-->
-<!--<html lang="en">-->
-<!--    <head>-->
-<!--        <meta charset="UTF-8">-->
-<!--        <meta name="viewport" content="width=device-width, initial-scale=1">-->
-<!--         <link href="normalize.css" rel="stylesheet" type="text/css">-->
-<!--     <title>My Mushroom Collection</title>-->
-<!--    </head>-->
-<!--    <body>-->
-<!---->
-<!--    </body>-->
-<!--</html>-->
+
