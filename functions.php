e6bc457e3e6bbf681b01322fe18be9b5bde93df9
James Taylor <taylor-j12@hotmail.com>
2019-09-24 10:48:10 +0100

    broke function into functions

--- functions.php
+++ functions.php
@@ -1,15 +1,21 @@
 <?php
 
-function displayData ()
+function databaseConnection()
 {
-    $db = new PDO('mysql:host=db; dbname=Collection','root','password');
+    $db = new PDO('mysql:host=db; dbname=Collection', 'root', 'password');
 
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
+    return $db;
+}
+function retrieveData($db)
+{
     $collection = $db->query('SELECT `Latin Name`, `Common Name`, `Height(cm)`, `Cap Width(cm)`, `Deadly?` FROM `Collection`');
 
     $allItems = $collection->fetchAll();
-
+    return $allItems;
+}
+function displayData($allItems)
+{
     foreach($allItems as $row)
     {
         echo '<div>';
